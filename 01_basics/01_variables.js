b6d7230df1397924f2c0cf7fc67a42219b0593f4
arnika242005 <arnikaraut24@gmail.com>
2025-03-22 17:39:19 +0000

    commit this

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 144553;
+let accountEmail = "arnika@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+console.log(accountEmail);
+
+accountEmail = "hc@hc.com"
+accountPassword = "123123"
+accountCity = "Mumbai"
