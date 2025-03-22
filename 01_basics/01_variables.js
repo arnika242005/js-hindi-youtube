6df821ea2f26c438ac07fa6b3cc3c3b3e25e4e47
arnika242005 <arnikaraut24@gmail.com>
2025-03-22 17:45:11 +0000

    third commit

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,10 +1,21 @@
-const accountId = 144553;
-let accountEmail = "arnika@google.com"
+const accountId = 144553; //const can’t be changed 
+let accountEmail = "arnika@google.com" //Can be reassigned but NOT redeclared in the same scope
 var accountPassword = "12345"
 accountCity = "Jaipur"
+let accountState; // will remain undefined 
 
-console.log(accountEmail);
 
-accountEmail = "hc@hc.com"
+// trying to change the upper lines and noticing the changes 
+// accountId = 2 //not allowed because const cant be changed hence not allowed
+accountEmail = "hc@hc.com" 
 accountPassword = "123123"
 accountCity = "Mumbai"
+
+
+console.log(accountId); //used to print any element i.e. one element. 
+
+
+console.log([accountId, accountEmail, accountPassword, accountCity]) //multiple elements at same time 
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity]) //prints in a table 
