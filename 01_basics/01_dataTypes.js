26ca2b0d91ae400ff51de3fe5e697c1a91b076b6
arnika242005 <arnikaraut24@gmail.com>
2025-03-29 05:57:56 +0000

    fifth last commit

--- 01_basics/01_dataTypes.js
+++ 01_basics/01_dataTypes.js
@@ -3,3 +3,24 @@
 // alert(3+3) //we are using nodejs, not browser 
 
 console.log(3+3) 
+
+console.log(3
+    +3) // code readability should be high 
+
+let name = "Hitesh"
+let age = 18 
+let isLoggedIn = false 
+
+//number => 2 to power 53
+//bigint 
+// string => " "
+//boolean => true/false
+//null => standalone value 
+// undefined =>
+// symbol => unique 
+
+// object 
+
+
+console.log(typeof undefined); // output is always undefined 
+console.log(typeof null); // type is object output is always null 
